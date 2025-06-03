1da4e93d3ed719da591e737b836ecf77e2f3fcf4
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2025-06-02 23:01:00 -0700

    Added Feign InventoryClient

--- src/main/java/com/jvel/microservices/order/service/OrderService.java
+++ src/main/java/com/jvel/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.jvel.microservices.order.service;
 
+import com.jvel.microservices.order.client.InventoryClient;
 import com.jvel.microservices.order.dto.OrderRequest;
 import com.jvel.microservices.order.model.Order;
 import com.jvel.microservices.order.repository.OrderRepository;
@@ -12,8 +13,15 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (!isProductInStock) {
+            throw new RuntimeException("Product with SkuCode" + orderRequest.skuCode() + " is not in stock.");
+        }
+
         // Map OrderRequest to Order object
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
