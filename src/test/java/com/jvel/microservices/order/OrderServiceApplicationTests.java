7a3868811dd1db973e7c887f7628061a0461f3e5
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2025-06-03 17:04:03 -0700

    Rotated password, moved secrest to .env and IntelliJ config

--- src/test/java/com/jvel/microservices/order/OrderServiceApplicationTests.java
+++ src/test/java/com/jvel/microservices/order/OrderServiceApplicationTests.java
@@ -1,15 +1,21 @@
 package com.jvel.microservices.order;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
 
 }
